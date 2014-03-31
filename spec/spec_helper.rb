3b1c9cbf84a2e177a817731c8c4eed4f34e5ff4b
Karl Freeman <karlfreeman@gmail.com>
2014-03-31 11:54:52 +0100

    tidy up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,23 +13,24 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
-end
 
-def jruby?
-  defined?(RUBY_ENGINE) && RUBY_ENGINE == 'jruby'
-end
+  def jruby?
+    defined?(RUBY_ENGINE) && RUBY_ENGINE == 'jruby'
+  end
 
-def undefine_constants(*consts)
-  values = {}
-  consts.each do |const|
-    if Object.const_defined?(const)
-      values[const] = Object.const_get(const)
-      Object.send :remove_const, const
+  def undefine_constants(*consts)
+    values = {}
+    consts.each do |const|
+      if Object.const_defined?(const)
+        values[const] = Object.const_get(const)
+        Object.send :remove_const, const
+      end
+    end
+    yield
+  ensure
+    values.each do |const, value|
+      Object.const_set const, value
     end
   end
-  yield
-ensure
-  values.each do |const, value|
-    Object.const_set const, value
-  end
+
 end
